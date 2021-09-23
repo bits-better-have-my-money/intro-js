3844ad0492fd9c5612f6710107b819b9e6d43fbf
dani <dani.zaghian@gmail.com>
2021-09-23 11:03:35 -0700

    adds differences

--- diff.js
+++ diff.js
@@ -1,108 +1,78 @@
-// # Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
-// def print_numbers_divisible_by_three
-//   index = 1
-//   while index <= 1000
-//     if index % 3 == 0
-//       puts index
-//     end
-//     index += 1
-//   end
-// end
-// print_numbers_divisible_by_three
-function printNumbersDivisibleByThree() {
-  var index = 1;
-  while (index <= 1000) {
-    if (index % 3 === 0) { 
-      console.log(index);
-    }
-    index += 1;
+// Equality check
+var x = 10;
+if (x === "10") {
+  console.log("equal");
+} else {
+  console.log("not equal");
+}
+
+// Falsey values
+var x = 3;
+if (null) {
+  x = x + 1;
+}
+if (false) {
+  x = x + 1;
+}
+if (0) {
+  x = x + 1;
+}
+if ("") {
+  x = x + 1;
+}
+console.log(x);
+
+// Functions
+function printLyrics() {
+  console.log("Now this is a story all about how");
+  console.log("My life got flipped turned upside down");
+  console.log("And I'd like to take a minute, just sit right there");
+  console.log("I'll tell you how I became the prince of a town called Bel-Air");
+}
+
+printLyrics();
+
+// Scope
+var x = 100;
+
+function addOne(num) {
+  x = 1;
+  return num + x;
+}
+
+console.log(x);
+console.log(addOne(5));
+console.log(x);
+
+// JavaScript objects vs. Ruby classes
+function Boat(name, color, price) {
+  this.name = name;
+  this.color = color;
+  this.price = price;
+  this.printInfo = function() {
+    console.log("The boat " + this.name + " has a price of " + this.price);
+  };
+}
+
+var boat = new Boat("S. S. Minnow", "white", 20000);
+console.log(boat);
+console.log(boat.name);
+boat.color = "red";
+console.log(boat.color);
+boat.printInfo();
+
+// Or just build the object directly
+var boat = {
+  name: "S. S. Minnow",
+  color: "white",
+  price: 20000,
+  printInfo: function() {
+    console.log("The boat " + this.name + " has a price of " + this.price);
   }
-}
-printNumbersDivisibleByThree();
+};
 
-
-// # Write a method that accepts an array of strings and prints out every other string.
-// def print_every_other_item(strings)
-//   index = 0
-//   strings.each do |string|
-//     if index % 2 == 0
-//       puts string
-//     end
-//     index += 1
-//   end
-// end
-// print_every_other_item(["a", "b", "c", "d", "e"])
-function printEveryOtherItem(strings) {
-  var index = 0;
-  strings.forEach(function(string) {
-    if (index % 2 === 0) {
-      console.log(string);
-    }
-    index += 1;
-  });
-}
-printEveryOtherItem(["a", "b", "c", "d", "e"]);
-
-
-// # Write a method that accepts an array of numbers and returns the sum.
-// def compute_sum(numbers)
-//   sum = 0
-//   numbers.each do |number|
-//     sum += number
-//   end
-//   return sum
-// end
-// puts compute_sum([2, 4, 5])
-function computeSum(numbers) {
-  var sum = 0;
-  numbers.forEach(function(number) {
-    sum += number;
-  });
-  return sum;
-}
-console.log(computeSum([2, 4, 5]));
-
-// # Start with the hash: city_populations = {chi: 2700000}
-// # Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
-// # The result should be: {chi: 2700000, nyc: 8400000, sf: 800000}
-// city_populations = {chi: 2700000}
-// city_populations[:nyc] = 8400000
-// city_populations[:sf] = 800000
-// p city_populations
-var cityPopulations = {chi: 2700000};
-cityPopulations["nyc"] = 8400000;
-cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
-console.log(cityPopulations);
-
-
-
-// # Write a method that prints out every number from 1 to 100. 
-
-
-// # Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
-
-
-// # Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
-
-
-// # Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string. 
-// # For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
-
-
-// # Start with the hash: item_amounts = {chair: 5, table: 2}
-// # Someone just bought two chairs. Change the hash such that the chair amount is 3.
-// # The final result should be: {chair: 3, table: 2}
-
-
-// # Start with the hash: item_amounts = {chair: 5, table: 2}
-// # You received 7 desks to sell. Change the hash to include desks.
-// # The final result should be: {chair: 5, table: 2, desk: 7}
-
-
-// # Write a method that accepts a number and returns its factorial.
-// # For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
-
-
-// # Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
-// # For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
-
+console.log(boat);
+console.log(boat.name);
+boat.color = "red";
+console.log(boat.color);
+boat.printInfo();
